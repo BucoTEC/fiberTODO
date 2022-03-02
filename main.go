2139688b8f1b2ac34bfcf71c0e8260b8a181df94
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-02 21:11:17 +0100

    added grouping

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"github.com/BucoTEC/golang-todo/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+//handler for routing
+func setupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+        return c.Status(fiber.StatusOK).JSON(fiber.Map{
+            "success":  true,
+            "message": "You are at the endpoint 😉",
+        })
+    })
+
+	api := app.Group("/api")
+	routes.TodoRoute(api.Group("/todos"))
+}
+
 func main(){
 	app:= fiber.New()
 	app.Use(logger.New())
-	app.Get("/", func (c *fiber.Ctx) error {
-		return c.Status(fiber.StatusOK).JSON("Hello from golang server")
-	})
+	
+	setupRoutes(app)
 
 	err := app.Listen(":5000")
 
